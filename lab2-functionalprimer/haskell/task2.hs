243b6203446b7b17f12e8dfa2e6075e14e9669f2
hadz1404 <210068968@aston.ac.uk>
2024-02-03 21:47:00 +0000

    Edited task2.hs in lab2-functionalprimer

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,9 @@
+
+onePlusone=putStrLn("1 + 1 = 2" ) -- create a function and make it contain a string using PutStrLine
 main = do
+    
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+   
